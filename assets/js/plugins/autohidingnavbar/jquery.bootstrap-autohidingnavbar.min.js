ce2cf071dba2908b4a0e0df225e41d966fe98304
Riccardo <pulver22@gmail.com>
2019-12-16 21:05:19 +0000

    HTTPS + working navbar

--- assets/js/plugins/autohidingnavbar/jquery.bootstrap-autohidingnavbar.min.js
+++ assets/js/plugins/autohidingnavbar/jquery.bootstrap-autohidingnavbar.min.js
@@ -1,7 +1,7 @@
 /*
  *  Bootstrap Auto-Hiding Navbar - v1.0.0
  *  An extension for Bootstrap's fixed navbar which hides the navbar while the page is scrolling downwards and shows it the other way. The plugin is able to show/hide the navbar programmatically as well.
- *  http://www.virtuosoft.eu/code/bootstrap-autohidingnavbar/
+ *  https://www.virtuosoft.eu/code/bootstrap-autohidingnavbar/
  *
  *  Made by István Ujj-Mészáros
  *  Under Apache License v2.0 License
